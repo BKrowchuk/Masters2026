086b34331f3aebcd8b22a0e8240a6d03b99aa9eb
benkrowchuk <162325063+benkrowchuk@users.noreply.github.com>
2026-03-26 08:01:05 -0400

    file paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Master2025/',
+  base: '/Masters2026/',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
